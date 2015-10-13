66abcb11c0fffa986407753e3b17529bd0d04c06
rafacorrea <rafael_correa_3@hotmail.com>
2015-10-13 02:33:27 -0500

    i

---
+++ examenRapido1/main.c
@@ -0,0 +1,126 @@
+//
+//  main.c
+//  factorial_sockets
+//
+//  Created by Vicente Cubells Nonell on 28/09/15.
+//  Copyright © 2015 Vicente Cubells Nonell. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define TCP_PORT 8000
+
+int factorial (int num);
+
+/* reverse:  reverse string s in place */
+void reverse(char s[])
+{
+    int i, j;
+    char c;
+    int numeros[20];
+    
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itoa(int n, char s[])
+{
+    int i, sign;
+    
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    struct sockaddr_in direccion;
+    char buffer[1000];
+    
+    int servidor, cliente;
+    
+    int leidos, escritos;
+    
+    if (argc != 2) {
+        printf("Error. Use: %s A.B.C.D \n", argv[0]);
+        exit(-1);
+    }
+    
+    //Crear el socket
+    servidor = socket(PF_INET, SOCK_STREAM, 0);
+    
+    // Enlace con el socket
+    direccion.sin_port = htons(TCP_PORT);
+    direccion.sin_family = AF_INET;
+    inet_aton(argv[1], &direccion.sin_addr);
+    
+    bind(servidor, (struct sockaddr *) &direccion, sizeof(direccion));
+    
+    // Escuchar a los clientes
+    listen(servidor, 10);
+    
+    // Comunicación
+    socklen_t tamano = sizeof(direccion);
+    
+    cliente = accept(servidor, (struct sockaddr *) &direccion, &tamano);
+    
+    int fact;
+    
+    if (cliente >= 0) {
+        printf("Aceptando conexiones en %s:%d \n",
+               inet_ntoa(direccion.sin_addr),
+               ntohs(direccion.sin_port));
+        
+        // Leer de socket y escribir en pantalla
+        while (leidos = read(cliente, &buffer, sizeof(buffer))) {
+            write(fileno(stdout), &buffer, leidos);
+            
+            printf("%d\n\n", atoi(buffer));
+            fact = factorial(atoi(buffer));
+            
+            printf("Factorial = %d\n", fact);
+            
+            itoa(fact, buffer);
+            
+           //leidos = write(fileno(stdin), &buffer, sizeof(buffer));
+            write(cliente, &buffer, sizeof(int));
+        }
+    }
+    
+    // Cerrar el socket
+    
+    close(servidor);
+    close(cliente);
+    
+    return 0;
+}
+
+int factorial (int num)
+{
+    int f = 1;
+    int i;
+    for(i = 2; i <= num; ++i)
+        f *= i;
+    
+    return f;
+}
+
