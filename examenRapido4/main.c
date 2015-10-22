cb8e711e33da1bf3f0dc54f614410ebc88c99659
Rafael Correa <rafael_correa_3@hotmail.com>
2015-10-22 15:24:53 -0500

    ok

--- examenRapido4/main.c
+++ examenRapido4/main.c
@@ -30,13 +30,21 @@ void * agente(void * arg)
         /* Inicia Región crítica */
         while(1)
         {
-            pthread_mutex_lock(&mutex); // DOWN
+            pthread_mutex_lock(&mutex);
+            while(pthread_mutex_trylock(&mutex2))
+            {
+                pthread_mutex_unlock(&mutex);
+            
+                pthread_mutex_lock(&mutex);
+            }//pthread_mutex_lock(&mutex); // DOWN
+            //printf("AGENTE!!\n");
             if(!fosforos && !papel && !tabaco)
             {
+                
                 temp = rand() % 3;
                 switch (temp)
                 {
-                    case 0: tabaco += 1; papel += 1; printf("[Agente]Se coloco papel y tabaco");break; 
+                    case 0: tabaco += 1; papel += 1; printf("[Agente]Se coloco papel y tabaco\n");break; 
                     case 1: tabaco += 1; fosforos += 1; printf("[Agente]Se coloco tabaco y fosforos\n");break;
                     case 2: papel += 1; fosforos += 1; printf("[Agente]Se coloco papel y fosforos\n"); break;
                 }
@@ -45,10 +53,11 @@ void * agente(void * arg)
                 /* Termina región crítica */
             }
             pthread_mutex_unlock(&mutex2); // UP
+            pthread_mutex_unlock(&mutex);
         }
     
     
-    pthread_exit(0);
+    //pthread_exit(0);
 }
 
 void * fumar(void * arg)
@@ -60,55 +69,66 @@ void * fumar(void * arg)
     
     while(1) {
         
-        /* Inicia Región crítica */
         pthread_mutex_lock(&mutex2);
+        /* Inicia Región crítica */
+        while(pthread_mutex_trylock(&mutex)){
+            pthread_mutex_unlock(&mutex2);
+            
+            pthread_mutex_lock(&mutex2);
+        
+        }//pthread_mutex_lock(&mutex2);
         switch (tid)
         {
+           
             case 0: 
                 if (papel && fosforos){
-                    printf("[Tabaco]]Fumando...\n");
-                    sleep (rand()%3);
+                    printf("[Tabaco]Fumando... Esperando 0-2 segundos...\n");
                     papel -= 1;
                     fosforos -= 1;
+                    sleep (rand()%3);
+                    
                 }
                 break;
             case 1:
                 if (tabaco && fosforos){
-                    printf("[Papel]Fumando...\n");
-                    sleep (rand()%3);
+                    printf("[Papel]Fumando... Esperando 0-2 segundos...\n");
+
                     tabaco-=1;
                     fosforos-=1;
+                    sleep (rand()%3);
                 }
                 break;
             case 2:
                 if (tabaco && papel){
-                    printf("[Fosforos]Fumando...\n");
-                    sleep (rand()%3);
+                    printf("[Fosforos]Fumando... Esperando 0-2 segundos...\n");
+                    
                     tabaco -= 1;
                     fosforos -= 1;
+                    sleep (rand()%3);
                 }
                     
         }
         pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex2);
     }
     
     
-    pthread_exit(0);
+    //pthread_exit(0);
 }
 
 int main(int argc, const char * argv[])
 {
     srand(time(NULL));
-    pthread_t * tid;
     
+    pthread_t * tid;
     tid = malloc(4 * sizeof(pthread_t));
     
     printf("Creando hilos ...\n");
     
     pthread_create(tid, NULL, fumar, (void *)0);
     pthread_create(tid+1, NULL, fumar, (void *)1);
-    pthread_create(tid+2, NULL, fumar, (void *)1);
-    pthread_create(tid+3, NULL, agente, (void *)1);
+    pthread_create(tid+2, NULL, fumar, (void *)2);
+    pthread_create(tid+3, NULL, agente, (void *)3);
     
     printf("Se crearon %d hilos ...\n", 4);
     /*
